6726e939c41aaa111aed9570cd65365ae11c58df
magicstunts123 <78170077+magicstunts123@users.noreply.github.com>
2022-10-21 19:46:38 +0200

    Added - Content and Input

---
+++ Source/RogueGame/RogueGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RogueGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROGUEGAME_API ARogueGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
